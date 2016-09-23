adb8adbd68a78e9f034d4f3613497b909e7c2f34
JobYegor <alohawind@mail.ru>
2016-09-23 15:56:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+require 'sendphpmailer.php';
+
+// mailer($name, $email, $sub, $text);
+mailer('EGORT', 'etikhanin@sspproma', 'TEST TEST TEST - ПРОВЕРКА ПРОВЕРКА ПРОВЕРКА', 'Here is text for test, and now test russian language -- просто тектс по-русски');
+
+?>
---
+++ sendphpmailer.php
@@ -0,0 +1,29 @@
+<?php
+
+function mailer($name, $email, $sub, $text) {
+
+	require 'PHPMailer/PHPMailerAutoload.php';
+
+	$mail = new PHPMailer;
+	$mail->isSMTP();
+	$mail->SMTPSecure = 'ssl';
+	$mail->SMTPAuth = true;
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 465;
+	$mail->Username = 'yegor.tikhanin@gmail.com';
+	$mail->Password = 'hamhamham';
+	$mail->setFrom('alohawind@mail.ru');
+	$mail->addAddress('soharta@list.ru');
+	$mail->Subject = 'Письмо от: '.$name;
+	// $mail->Body = 'Письмо';
+	$mail->Body = 'Письмо от: '.$name.'<br>'.'Обратный адрес: '.$email.'<br>'.$text;
+	//send the message, check for errors
+	if (!$mail->send()) {
+	    echo "ERROR: " . $mail->ErrorInfo;
+	} else {
+	    echo "SUCCESS2";
+	}
+
+}
+
+?>
